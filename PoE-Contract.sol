3b2cfb1cf69a67b74bcee67bc6f5718679a72273
softdevad <diemertanthony06@gmail.com>
2022-03-16 01:01:01 +0100

    update PoE and README

--- PoE-Contract.sol
+++ PoE-Contract.sol
@@ -12,66 +12,79 @@ pragma solidity >=0.7.0 <0.9.0;
  */
 
 contract ProofOfExistence {
-
     address admin;
+
     constructor(address _admin) {
         admin = _admin;
     }
 
-    modifier onlyAdmin {
+    modifier onlyAdmin() {
         require(msg.sender == admin, "Insufficient access");
         _;
     }
 
-    struct CertificateDetails {  
-       uint certificateID;
-       string candidateName;
-       string courseName;
-       string grade;
-       uint date;
+    struct CertificateDetails {
+        uint256 certificateID;
+        string candidateName;
+        string courseName;
+        string grade;
+        uint256 date;
     }
 
-    uint certificateID = 200;
+    uint256 certificateID = 200;
 
-    mapping(address => uint) getID;
-    mapping(uint => CertificateDetails) storeID;
+    mapping(address => uint256) getID;
+    mapping(uint256 => CertificateDetails) storeID;
 
-    CertificateDetails[] certificateRecord;       
+    CertificateDetails[] certificateRecord;
 
-    function proofCertificateExist (
-      address _candidate,
-      uint memory _certificateID,
-      string memory _candidateName,
-      string memory _courseName,
-      string memory _grade,
-      uint memory _date
-        ) public onlyAdmin returns(bool, uint) {
-            
-            if(certificateIdTaken(_candidate)) {
-                return (false, getID[_candidate]);
-            }
-            certificateID = certificateID + 1;
+    function proofCertificateExist(
+        address _candidate,
+        uint256 _certificateID,
+        string memory _candidateName,
+        string memory _courseName,
+        string memory _grade,
+        uint256 _date
+    ) public onlyAdmin returns (bool, uint256) {
+        if (certificateIdTaken(_candidate)) {
+            return (false, getID[_candidate]);
+        }
+        certificateID = certificateID + 1;
 
-            CertificateDetails memory newCertificate = CertificateDetails(_certificateID, _candidateName, _courseName, _grade, _date);
+        CertificateDetails memory newCertificate = CertificateDetails(
+            _certificateID,
+            _candidateName,
+            _courseName,
+            _grade,
+            _date
+        );
 
-            certificateRecord.push(newCertificate);
+        certificateRecord.push(newCertificate);
 
-            getID[_candidate] = certificateID;
-            storeID[certificateID] = newCertificate;
-            return(true, certificateID);
+        getID[_candidate] = certificateID;
+        storeID[certificateID] = newCertificate;
+        return (true, certificateID);
     }
 
-    function verifyId(uint _certificateID) external view returns(CertificateDetails memory) {
+    function verifyId(uint256 _certificateID)
+        external
+        view
+        returns (CertificateDetails memory)
+    {
         return storeID[_certificateID];
     }
 
-    function retrieveID() external view returns(bool, uint) {
-        uint id = getID[msg.sender];
-        if(id == 0) return (false, 0);
+    function retrieveID() external view returns (bool, uint256) {
+        uint256 id = getID[msg.sender];
+        if (id == 0) return (false, 0);
         return (true, id);
     }
-    
-    function certificateIdTaken(address _candidate) internal view returns(bool) {
+
+    function certificateIdTaken(address _candidate)
+        internal
+        view
+        returns (bool)
+    {
         return !(getID[_candidate] == 0);
     }
 }
--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-# Awesome-Smart-Contracts
+# Awesome Smart Contract
 
-A personal challenge on simple smart contracts built using solidity programming language.
+[![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/sindresorhus/awesome)  
 
-🎯 My aim to get better with Solidity, Ethereum development tools and how decentralized applications validate transactions without trusted authorities/third parties.
+Personal Challenge on simple smart contracts development. 
+
+🎯 My aim to get better with Solidity, Ethereum development tools and mode of operation of decentralized applications.
 
 ## Project Description
 
-These Smart contracts are built using [Solidity](https://docs.soliditylang.org) on Ethereum Virtual Machine.
+These Smart contracts were created using [Solidity](https://docs.soliditylang.org) on Ethereum Virtual Machine.
 
 ## Getting Started
 
-Run each smart contract in a different workspace/directory on [Remix IDE](https://remix.ethereum.org/)
-
+Run each smart contract in a different workspace/directory on [Remix IDE](https://remix.ethereum.org/).
